08f7397387a70b34e78dac36ae4d5c0f23c2ddc5
Charles Samonte <cksamonte@gmail.com>
2026-01-03 23:02:45 -0600

    Fixed styling issues in Contact.

--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -10,7 +10,7 @@ const OurServices = () => {
             <div className="services-container">
                 <div className="service-card">
                     <div className="img-container">
-                        <img src="/src/assets/services/auto-body-repair.png" alt="Auto Body Repair">
+                        <img loading="lazy" src="/src/assets/services/auto-body-repair.png" alt="Auto Body Repair">
                         </img>
                     </div>
                     <h3>Automotive Services</h3>
@@ -18,14 +18,14 @@ const OurServices = () => {
                 </div>
                 <div className="service-card">
                     <div className="img-container">
-                        <img src="/src/assets/services/engine-repair.png" alt="Engine Repair"></img>
+                        <img loading="lazy" src="/src/assets/services/engine-repair.png" alt="Engine Repair"></img>
                     </div>
                     <h3>Engine Repair</h3>
                     <p>Professional engine diagnostics and repair services to keep your vehicle running smoothly.</p>
                 </div>
                 <div className="service-card">
                     <div className="img-container">
-                        <img src="/src/assets/services/4x4-setup.png" alt="4x4setup"></img>
+                        <img loading="lazy" src="/src/assets/services/4x4-setup.png" alt="4x4setup"></img>
                     </div>
                     <h3>4x4 Setup and Overloading Setup</h3>
                     <p>Tailored fabrication solutions to meet your unique vehicle needs and specifications.</p>
